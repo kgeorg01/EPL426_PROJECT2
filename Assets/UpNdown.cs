db24d3d2534607f46c2a1aaf26d9e34131da2157
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-03-27 00:18:49 +0200

    commit 1

---
+++ Assets/UpNdown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpNdown : MonoBehaviour
+{
+     public AnimationCurve myCurve;
+    private float height;
+    private void Start()
+    {
+        height = transform.position.y;
+    }
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, height + myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }
+}
