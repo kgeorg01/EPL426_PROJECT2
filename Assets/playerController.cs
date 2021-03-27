798c4d573bc06b8e4ed3ba9451af2c72d83b3aaa
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-03-27 11:53:53 +0200

    commit 2

---
+++ Assets/playerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Animator anim;
+    public Transform pos;
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Rotate(Vector3.up, 45 * Time.deltaTime);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Rotate(Vector3.up, -45 * Time.deltaTime);
+        }
+    }
+}
