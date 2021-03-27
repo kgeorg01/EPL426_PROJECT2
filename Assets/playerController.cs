927c71692ddc5c37f47c04da3c19a536cf47425a
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-03-27 16:34:54 +0200

    Continuous Movement

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Animator anim;
-    public Transform pos;
+    public float speed = 5;
+   
     // Update is called once per frame
     void Update()
     {
         anim.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
+        if (Input.GetAxisRaw("Horizontal") > 0){
             transform.Rotate(Vector3.up, 45 * Time.deltaTime);
         }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
+        else if (Input.GetAxisRaw("Horizontal") < 0) {
             transform.Rotate(Vector3.up, -45 * Time.deltaTime);
         }
+        else if(Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.position += Vector3.forward * 5 * Time.deltaTime;
+
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.position += -Vector3.forward * 2 * Time.deltaTime;
+
+        }
     }
+
+   
 }
