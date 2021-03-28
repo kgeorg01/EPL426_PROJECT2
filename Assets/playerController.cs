edc4809fa5da8a5ec5486258b0202206e03aca33
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-03-29 00:35:21 +0300

    Second try at animations (InPlace)

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -5,28 +5,19 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Animator anim;
-    public float speed = 5;
-   
-    // Update is called once per frame
+
+
     void Update()
     {
         anim.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+        
         if (Input.GetAxisRaw("Horizontal") > 0){
-            transform.Rotate(Vector3.up, 45 * Time.deltaTime);
+            transform.Rotate(Vector3.up, 90 * Time.deltaTime);
         }
         else if (Input.GetAxisRaw("Horizontal") < 0) {
-            transform.Rotate(Vector3.up, -45 * Time.deltaTime);
+            transform.Rotate(Vector3.up, -90 * Time.deltaTime);
         }
-        else if(Input.GetAxisRaw("Vertical") > 0)
-        {
-            transform.position += Vector3.forward * 5 * Time.deltaTime;
-
-        }
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            transform.position += -Vector3.forward * 2 * Time.deltaTime;
-
-        }
+       
     }
 
    
