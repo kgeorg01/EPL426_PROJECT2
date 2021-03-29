59a5992810f5286e9b5ed7007e3abdea2a9a74ab
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-03-29 13:18:51 +0300

    Moving and Jumping

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,21 +4,61 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public Animator anim;
+    private Animator anim;
+    private bool grounded;
+    private bool running;
+    private Rigidbody rb;
+    private float jump;
+    private float speed;
+    private Vector3 movement;
 
-
-    void Update()
+    private void Start()
     {
-        anim.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
-        
-        if (Input.GetAxisRaw("Horizontal") > 0){
+        rb = gameObject.GetComponent<Rigidbody>();
+        anim = gameObject.GetComponent<Animator>();
+        grounded = true;
+        jump = 5;
+        speed = 5;
+    }
+   
+    void FixedUpdate()
+    {
+        float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+        movement.Set(mH, 0f, mV);
+        movement = movement.normalized * speed * Time.deltaTime;
+        movement = transform.position + (transform.forward * movement.z) + (transform.right * movement.x);
+
+        if (mV > 0) {
+            rb.MovePosition(movement);
+        }
+        else if (mV < 0)
+        {
+            rb.MovePosition(movement);
+        }
+
+        if (mH > 0) {
             transform.Rotate(Vector3.up, 90 * Time.deltaTime);
         }
-        else if (Input.GetAxisRaw("Horizontal") < 0) {
+        else if (mH < 0) {
             transform.Rotate(Vector3.up, -90 * Time.deltaTime);
         }
-       
+
+        anim.SetFloat("vertical", mV);
+        anim.SetFloat("horizontal", mH);
+        if (Input.GetKeyDown("space")) { 
+            if (grounded) { 
+                anim.SetTrigger("jump");
+                rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+                grounded = false;
+            }
+        }
     }
 
-   
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.rigidbody.tag.Equals("Ground"))
+        {
+            grounded = true;
+        }
+    }
 }
