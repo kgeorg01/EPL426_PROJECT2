2d73d1db3eae0ad75428cce04573ca8d32bde18c
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-03-30 18:07:41 +0300

    Collectibles, Palatin N Collectibles Audio

--- Assets/UpNdown.cs
+++ Assets/UpNdown.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class UpNdown : MonoBehaviour
 {
-     public AnimationCurve myCurve;
+    public AnimationCurve myCurve;
     private float height;
+    private float rand;
     private void Start()
     {
         height = transform.position.y;
+        rand = Random.Range(0f, 5f);
     }
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, height + myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+        transform.position = new Vector3(transform.position.x, height + myCurve.Evaluate(((Time.time+rand) % myCurve.length)), transform.position.z);
     }
 }
--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -11,6 +11,12 @@ public class playerController : MonoBehaviour
     private float jump;
     private float speed;
     private Vector3 movement;
+    public AudioSource walk;
+    public AudioSource jumpaudio;
+    public AudioSource armouraudio;
+    public int health = 100;
+    public int armour = 50;
+    public int gold = 0;
 
     private void Start()
     {
@@ -28,37 +34,77 @@ public class playerController : MonoBehaviour
         movement.Set(mH, 0f, mV);
         movement = movement.normalized * speed * Time.deltaTime;
         movement = transform.position + (transform.forward * movement.z) + (transform.right * movement.x);
-
+        
         if (mV > 0) {
             rb.MovePosition(movement);
+            if (!walk.isPlaying) walk.Play();
         }
-        else if (mV < 0)
-        {
+        else if (mV < 0) {
             rb.MovePosition(movement);
+            if (!walk.isPlaying) walk.Play();
+        }
+        else {
+            if (walk.isPlaying) walk.Stop();
         }
 
-        if (mH > 0) {
-            transform.Rotate(Vector3.up, 90 * Time.deltaTime);
+        if (mH > 0) transform.Rotate(Vector3.up, 90 * Time.deltaTime);
+        else if (mH < 0) transform.Rotate(Vector3.up, -90 * Time.deltaTime);
+        
+        if (mH != 0 || mV != 0)
+        {
+            if (!armouraudio.isPlaying) armouraudio.Play();
         }
-        else if (mH < 0) {
-            transform.Rotate(Vector3.up, -90 * Time.deltaTime);
-        }
-
+        else armouraudio.Pause();
         anim.SetFloat("vertical", mV);
         anim.SetFloat("horizontal", mH);
         if (Input.GetKeyDown("space")) { 
             if (grounded) { 
                 anim.SetTrigger("jump");
+                jumpaudio.Play();
                 rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
                 grounded = false;
             }
         }
+        if (jumpaudio.isPlaying) walk.Stop(); 
     }
 
     private void OnCollisionEnter(Collision collision) {
-        if (collision.rigidbody.tag.Equals("Ground"))
-        {
+        if (collision.rigidbody.tag.Equals("Ground")) {
             grounded = true;
         }
+        
     }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "HealthPotion") {
+            AudioSource audio = col.gameObject.GetComponent<AudioSource>();
+            AudioSource.PlayClipAtPoint(audio.clip, this.gameObject.transform.position);
+            Destroy(col.gameObject);
+            health = health + 50;
+        }
+        if (col.tag == "Coin")
+        {
+            AudioSource audio = col.gameObject.GetComponent<AudioSource>();
+            AudioSource.PlayClipAtPoint(audio.clip, this.gameObject.transform.position);
+            Destroy(col.gameObject);
+            gold = gold + 1;
+        }
+        if (col.tag == "Ingot")
+        {
+            AudioSource audio = col.gameObject.GetComponent<AudioSource>();
+            AudioSource.PlayClipAtPoint(audio.clip, this.gameObject.transform.position);
+            Destroy(col.gameObject);
+            gold = gold + 5;
+        }
+        if (col.tag == "Shield")
+        {
+            AudioSource audio = col.gameObject.GetComponent<AudioSource>();
+            AudioSource.PlayClipAtPoint(audio.clip, this.gameObject.transform.position);
+            Destroy(col.gameObject);
+            armour = armour + 25;
+        }
+    }
+    
+
 }
