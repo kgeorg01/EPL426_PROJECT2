10d21d09edf6c57e3fa90723b25ed0e2ae4e71b3
kgeorg01 <georgiou.p.kyriakos@ucy.ac.cy>
2021-04-08 14:23:23 +0300

    Revert "Tidying Assets/ AllStuffScene/ Save 0.41"
    This reverts commit f1123253c818a9eb5c3e0daab4ae5a4ad6ba07cc.

--- Assets/enemyVariables.cs
+++ Assets/enemyVariables.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class enemyVariables : MonoBehaviour
 {
-    public int health = 100;
+    public int health;
     public static bool attacking = false;
     public HUDHealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
     }
 
     // Update is called once per frame
