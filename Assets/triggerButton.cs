c481656a6dd139ccc2a01d9a69b5558e3ea600bd
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-04-09 10:58:13 +0300

    Gate that opens with button

---
+++ Assets/triggerButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerButton : MonoBehaviour
+{
+    public Animator gate;
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameObject.GetComponent<Animator>().Play("button");
+        Invoke("openGate", 2);
+    }
+
+    void openGate()
+    {
+        gate.Play("gate");
+    }
+    
+}
