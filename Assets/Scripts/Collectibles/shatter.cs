fcd8d5517bcb9bafe7f9b8d05de155efbd79bcb7
kgeorg01 <georgiou.p.kyriakos@ucy.ac.cy>
2021-04-14 13:32:33 +0300

    Level1Forest v0.4 (Cave,Pondv2)

--- Assets/Scripts/Collectibles/shatter.cs
+++ Assets/Scripts/Collectibles/shatter.cs
@@ -6,12 +6,12 @@ public class shatter : MonoBehaviour
 {
     public GameObject broken;
     public GameObject gold;
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Player"))
+        if (collision.gameObject.tag.Equals("Player") &&  playerVariables.attacking)
         {
-            Instantiate(broken, transform.position, transform.rotation);
-            Instantiate(gold, transform.position + Vector3.up * 0.8f, gold.transform.rotation);
+            if (broken!= null) Instantiate(broken, transform.position, transform.rotation);
+            if (gold!= null) Instantiate(gold, transform.position + Vector3.up * 0.8f, gold.transform.rotation);
             Destroy(gameObject);
         }
     }
