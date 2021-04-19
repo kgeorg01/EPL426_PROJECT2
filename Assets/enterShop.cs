7b09635519d964959db6c4c06408fc8dc7c21a0c
tsolom01 <55096615+tsolom01@users.noreply.github.com>
2021-04-19 23:58:25 +0300

    Shop Prefab, Added to all scenes, Coins at stage 4, fix on lava stage 2

---
+++ Assets/enterShop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enterShop : MonoBehaviour
+{
+    private bool active = false;
+    public GameObject shop;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!false && other.tag.Equals("Player"))
+        {
+            active = true;
+            shop.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        Invoke("setFalse", 5f);
+    }
+
+    void setFalse()
+    {
+        active = false;
+    }
+}
--- Assets/opendoor.cs
+++ Assets/opendoor.cs
@@ -15,4 +15,5 @@ public class opendoor : MonoBehaviour
             gate.Play("Open");
         }
     }
+
 }
